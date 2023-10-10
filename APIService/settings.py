738ac5708912dbc0fda322687c7e9f336afb4037
Munkoor <eee1lvlboss@gmail.com>
2023-10-10 11:21:26 +0300

    Created JWT Authentication

--- APIService/settings.py
+++ APIService/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,6 +133,9 @@ REST_FRAMEWORK = {
         'user': '1/day'
     },
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 SPECTACULAR_SETTINGS = {
@@ -146,4 +149,10 @@ SPECTACULAR_SETTINGS = {
         'defaultModelsExpandDepth': 2,
         'defaultModelExpandDepth': 2,
     }
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKEN": True,
 }
